0fbc53f2a4c5dd2eefabf6517aeb24d44f90e2a9
Joaquim Moreno Prusi <jmprusi@keepalive.io>
2018-03-26 19:41:02 +0200

    Makefile: Use gnu-time only under linux
    Due to differences between bsd time and gnu time command,
    the current makefile files to run under Darwin.
    
    This commit disables writing "bench.txt" for non linux systems.

--- script/make_report_time.sh
+++ script/make_report_time.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 shift  # get rid of the '-c' supplied by make.
 
-\time -o bench.txt -a -f "[%E] : $*" -- sh -c "$*"
+if [ "$(uname)" = "Linux" ]; then
+        \command time -o bench.txt -a -f "[%E] : $*" -- sh -c "$*"
+else
+        # As other commands refer to the bench.txt file, we create it
+        # to avoid failures.
+        touch bench.txt
+        \command time sh -c "$*"
+fi
